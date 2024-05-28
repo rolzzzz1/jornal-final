20d1dfce79b5ae0e82617b976bdee62dc3b9c38f
Aishwarya Sharma <roli.mradul@gmail.com>
2024-05-28 12:31:10 +0530

    fix

--- index.js
+++ index.js
@@ -7,15 +7,15 @@ const recentPostTitle = document.getElementById("recentPostTitle");
 const extraPosts = document.getElementById("extraPosts");
 var isHome = true;
 
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", mobileMenu);
+// hamburger.addEventListener("click", mobileMenu);
 
-function mobileMenu() {
-  hamburger.classList.toggle("active");
-  navMenu.classList.toggle("active");
-}
+// function mobileMenu() {
+//   hamburger.classList.toggle("active");
+//   navMenu.classList.toggle("active");
+// }
 
 // Hero div event listener
 heroDiv.addEventListener("click", function () {
