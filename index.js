048a41392ffa101da1761cbf0c8b9ac59e24a6f8
Aishwarya Sharma <roli.mradul@gmail.com>
2024-05-30 13:04:50 +0530

    Update index.js

--- index.js
+++ index.js
@@ -115,3 +115,24 @@ viewBtn.addEventListener("click", function () {
 
   extraPosts.classList.toggle("hidden");
 });
+
+// Nav
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach((n) => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}
